f4c66e867a5bdab4c807bb4770e5c18145d02141
David Wan <dave.wan@mail.utoronto.ca>
2018-11-14 10:59:19 -0500

    Added basic homepage and intro banner component

---
+++ david-wan/src/components/IntroBanner.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class IntroBanner extends Component {
+  render() {
+    return(
+      <div className="introbanner">
+        
+      </div>
+    );
+  }
+}
+
+export default IntroBanner
