16b4aabc1b8f94672a99cb649aaf7841c40db48a
David Wan <dave.wan@mail.utoronto.ca>
2019-05-02 11:01:08 -0400

    Added grid styling to Works

--- david-wan/src/pages/Works.js
+++ david-wan/src/pages/Works.js
@@ -1,11 +1,22 @@
 import React, {Component} from "react";
+import '../style/Works.css'
 
 class Works extends Component {
   render() {
     return(
       <div className="works">
         <h1>These are my works!</h1>
-
+        <div className="works-content">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     )
   }
