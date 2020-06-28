3db99f054d51ef26ee9859522ec793e3e121420e
David Wan <David.Wan@ibm.com>
2020-06-27 20:38:30 -0400

    Added basic pages

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../style/AboutMe.css'
+
+export default function AboutMe() {
+    return (
+        <div id="about-me">
+            <div>
+                <b class="title">Who am I?</b>
+                <p>
+                    I am a 4th year Computer Science Student at the University of Toronto.
+                    I have interned at various companies, including IBM as a Full Stack Developer.
+                </p>
+            </div>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,33 @@
 import React from "react"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+import Home from './Home';
+import AboutMe from './AboutMe'
 
-export default function Home() {
-  return <div>Hello world!</div>
+import '../style/App.css'
+
+export default function App() {
+  return (
+    <Router>
+      <nav id="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/aboutme">About Me</Link>
+      </nav>
+
+      <div id='content-container'>
+        <Switch>
+          <Route path="/aboutme">
+            <AboutMe />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
