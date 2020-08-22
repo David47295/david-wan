06cf3b5c3bfa150c55f14df2d705aa71c1fb9e41
David <davidwan97@gmail.com>
2020-08-22 18:43:36 -0400

    Added banner to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import '../style/Home.css'
+
 export default function Home() {
     return (
-        <div class="home-banner"></div>
+        <div class="home-banner">
+            <div class="banner-content">
+                <h1>David Wan</h1>
+                <hr/>
+                <div class="self-descrip">
+                    <p>Software Development</p>
+                    <p>Computer Science Specialist</p>
+                    <p>Information Systems</p>
+                </div>
+            </div>   
+        </div>
         
     )
 }
