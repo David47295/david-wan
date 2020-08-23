f082fba1911026ba3349962111539220b5c0f2a7
David <davidwan97@gmail.com>
2020-08-22 20:34:40 -0400

    Added some temporary styling

--- src/components/home/Skills.js
+++ src/components/home/Skills.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import '../../style/Skills.css'
+
 export default function Skills() {
     return (
         <div id="skills">
             <h1>Skills</h1>
             <div id="skills-container">
                 <div>
-                    
+                    <div class="ue4"></div>
+                    <p>Game Development</p>
                 </div>
             </div>
         </div>
