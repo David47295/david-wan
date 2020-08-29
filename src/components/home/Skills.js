acb715930ad7c096ed13669a01797f71cf0406a7
David <davidwan97@gmail.com>
2020-08-29 18:20:20 -0400

    Added more skills

--- src/components/home/Skills.js
+++ src/components/home/Skills.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import SkillCell from '../skills/SkillCell'
+import data from '../../skills.json'
 
 import '../../style/Skills.css'
 
 export default function Skills() {
+    const skillsData = data.skills;
+    const skillCells = skillsData.map((skill, i) => {
+        return (
+            <SkillCell
+                key={i}
+                skillId={skill.id}
+                captionText={skill.captionText}
+            />
+        )
+    })
     return (
         <div id="skills">
             <h1>Skills</h1>
             <div id="skills-container">
-                <SkillCell
-                    skillId="ue4"
-                    captionText="Game Development"
-                />
-                {/* <SkillCell
-                    skillId="web-dev"
-                    captionText="Web Development"
-                /> */}
+                {skillCells}
             </div>
         </div>
     )
--- src/components/skills/SkillCell.js
+++ src/components/skills/SkillCell.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function SkillCell(props) {
     return (
-        <div id={props.skillId}>
+        <div id={props.skillId} className="skill-icon">
             <p className="skills-caption">{props.captionText}</p>
         </div>
     );
