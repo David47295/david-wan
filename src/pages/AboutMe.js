2e74af96575849552031188f24d4d38c2bded179
David Wan <davidwan97@gmail.com>
2020-11-05 23:59:03 -0500

    Added more to About Me description

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,10 +6,17 @@ export default function AboutMe() {
         <div id="about-me">
             <div>
                 <b className="title">Who am I?</b>
-                <p>
-                    I am a 4th year Computer Science Student at the University of Toronto.
-                    I have interned at various companies, including IBM as a Full Stack Developer.
-                </p>
+                <div id="about-me-content">
+                    <div>
+                        Hey there! Thanks for taking the time to learn more about me. 
+                        My name is David Wan and I'm a 4th year Computer Science Student at the University of Toronto!
+                    </div>
+                    <div>
+                        I specialize in Web Development as a Full Stack Developer, and have tons of experience with various tools and frameworks.
+                        Some examples include React JS, Vue JS, SCSS, CSS, Node JS, Express JS, and Sockets.
+                        I also have plenty of experience working with various database technologies like PostgreSQL, MongoDB, and IBM DB2!
+                    </div>
+                </div>
             </div>
         </div>
     )
