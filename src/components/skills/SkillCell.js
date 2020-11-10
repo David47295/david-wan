daecb9935f599a7a065788f545bdbed6d2dae758
David Wan <davidwan97@gmail.com>
2020-11-09 23:05:29 -0500

    Removed old assets

--- src/components/skills/SkillCell.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function SkillCell(props) {
-    return (
-        <div id={props.skillId} className="skill-icon">
-            <p className="skills-caption">{props.captionText}</p>
-        </div>
-    );
-}
