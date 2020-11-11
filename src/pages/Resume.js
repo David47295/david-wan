1b5d9d66f662b737c0df05e29ee14315784d7a30
David Wan <davidwan97@gmail.com>
2020-11-10 23:36:00 -0500

    Added work experience entry

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../style/Resume.css'
 
 export default function Resume() {
     return (
         <div id="resume">
-            <b>Work Experience</b>
+            <b class="main-header">Work Experience</b>
+            <div class="work-experience">
+                <b class="role">Full Stack Developer (Cloud)</b>
+                <p class="location">IBM Canada – Markham, ON (May 2019 – August 2020)</p>
+                <ul>
+                    <li>Helped migrate older Operations Dashboard over to newer, micro-services and Cognos based architecture</li>
+                    <li>Developed new dashboard displaying breakdowns of various support KPIs that are tracked by numerous product leaders, using Vue JS, Node JS, and PostgreSQL</li>
+                    <li>Rapidly learned and utilized Cognos Analytics to develop several required dashboards within the team’s tight schedule</li>
+                    <li>Wrote styling guide for DashboardNEXT to ensure harmonized design across all tabs within the dashboard</li>
+                </ul>
+            </div>
         </div>
     )
 }
